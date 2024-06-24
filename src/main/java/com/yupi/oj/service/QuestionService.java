7df5de9cda6640c6c5a8608dadf69917298f18dc
Eileen Gu <81970016+ygu-hub@users.noreply.github.com>
2024-06-24 22:11:58 +0800

    问题业务层，与mapper持久层交互来处理控制器端要实现的增删改查效果

--- src/main/java/com/yupi/oj/service/QuestionService.java
+++ src/main/java/com/yupi/oj/service/QuestionService.java
@@ -1,7 +1,14 @@
 package com.yupi.oj.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.oj.model.dto.question.QuestionQueryRequest;
+import com.yupi.oj.model.entity.Question;
 import com.yupi.oj.model.entity.Question;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.oj.model.vo.QuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author guyingfan
@@ -9,5 +16,45 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2024-06-19 18:45:30
 */
 public interface QuestionService extends IService<Question> {
+    /**
+     * 校验
+     *
+     * @param question
+     * @param add
+     */
+    void validQuestion(Question question, boolean add);
 
+    /**
+     * 获取查询条件
+     *
+     * @param questionQueryRequest
+     * @return
+     */
+    QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest);
+
+//    /**
+//     * 从 ES 查询
+//     *
+//     * @param questionQueryRequest
+//     * @return
+//     */
+//    Page<Question> searchFromEs(QuestionQueryRequest questionQueryRequest);
+
+    /**
+     * 获取题目封装
+     *
+     * @param question
+     * @param request
+     * @return
+     */
+    QuestionVO getQuestionVO(Question question, HttpServletRequest request);
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionPage
+     * @param request
+     * @return
+     */
+    Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request);
 }
