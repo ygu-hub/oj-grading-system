9b54f20822008e640abfdf4637085e5fdd6d59bb
Eileen Gu <81970016+ygu-hub@users.noreply.github.com>
2024-06-24 22:08:37 +0800

    问题提交增查控制器入口

---
+++ src/main/java/com/yupi/oj/controller/QuestionSubmitController.java
@@ -0,0 +1,85 @@
+package com.yupi.oj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.oj.annotation.AuthCheck;
+import com.yupi.oj.common.BaseResponse;
+import com.yupi.oj.common.ErrorCode;
+import com.yupi.oj.common.ResultUtils;
+import com.yupi.oj.constant.UserConstant;
+import com.yupi.oj.exception.BusinessException;
+import com.yupi.oj.exception.ThrowUtils;
+import com.yupi.oj.model.dto.question.QuestionQueryRequest;
+import com.yupi.oj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.yupi.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
+import com.yupi.oj.model.entity.Question;
+import com.yupi.oj.model.entity.QuestionSubmit;
+import com.yupi.oj.model.entity.User;
+import com.yupi.oj.model.enums.UserRoleEnum;
+import com.yupi.oj.model.vo.QuestionSubmitVO;
+import com.yupi.oj.model.vo.QuestionVO;
+import com.yupi.oj.service.QuestionSubmitService;
+import com.yupi.oj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 问题提交接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/question_submit")
+@Slf4j
+public class QuestionSubmitController {
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 提交题目
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return
+     */
+    @PostMapping()
+    public BaseResponse<Long> doSubmitQuestion(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+            HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long result = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 分页获取题目提交列表
+     * 仅本人和管理员能够看到题目提交代码
+     * @param questionSubmitQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/list/page/vo")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitVOByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                         HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        User loginUser = userService.getLoginUser(request);
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        // 从数据库中先获取所有满足条件的题目提交信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        // 返回题目提交脱敏后的信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
+
+}
