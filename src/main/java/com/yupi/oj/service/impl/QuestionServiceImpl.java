ff126a51a609c039a696fad85f1ed34b1b23b769
Eileen Gu <81970016+ygu-hub@users.noreply.github.com>
2024-06-24 22:13:20 +0800

    业务层实现类

--- src/main/java/com/yupi/oj/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/yupi/oj/service/impl/QuestionServiceImpl.java
@@ -1,10 +1,29 @@
 package com.yupi.oj.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.yupi.oj.model.entity.Question;
+import com.yupi.oj.common.ErrorCode;
+import com.yupi.oj.constant.CommonConstant;
+import com.yupi.oj.exception.BusinessException;
+import com.yupi.oj.exception.ThrowUtils;
+import com.yupi.oj.model.dto.question.QuestionQueryRequest;
+import com.yupi.oj.model.entity.*;
+import com.yupi.oj.model.vo.QuestionVO;
+import com.yupi.oj.model.vo.UserVO;
 import com.yupi.oj.service.QuestionService;
 import com.yupi.oj.mapper.QuestionMapper;
+import com.yupi.oj.service.UserService;
+import com.yupi.oj.utils.SqlUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
 * @author guyingfan
@@ -14,7 +33,266 @@ import org.springframework.stereotype.Service;
 @Service
 public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question>
     implements QuestionService{
+    @Resource
+    private UserService userService;
 
+    /**
+     * 校验题目是否合法
+     * @param question
+     * @param add
+     */
+    @Override
+    public void validQuestion(Question question, boolean add) {
+        if (question == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+
+        String title = question.getTitle();
+        String content = question.getContent();
+        String answer = question.getAnswer();
+        String tags = question.getTags();
+        String judgeCase = question.getJudgeCase();
+        String judgeConfig = question.getJudgeConfig();
+
+        // 创建时，参数不能为空
+        if (add) {
+            ThrowUtils.throwIf(StringUtils.isAnyBlank(title, content, tags), ErrorCode.PARAMS_ERROR);
+        }
+        // 有参数则校验
+        if (StringUtils.isNotBlank(title) && title.length() > 80) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "标题过长");
+        }
+        if (StringUtils.isNotBlank(content) && content.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+        if (StringUtils.isNotBlank(answer) && answer.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+        if (StringUtils.isNotBlank(judgeCase) && judgeCase.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+        if (StringUtils.isNotBlank(judgeConfig) && judgeConfig.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+    }
+
+    /**
+     * 获取查询包装类 (根据前端传来的请求对象，得到 mybatis 框架支持的查询包装类（queryWrapper类）)
+     *
+     * @param questionQueryRequest
+     * @return
+     */
+    @Override
+    public QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest) {
+        QueryWrapper<Question> queryWrapper = new QueryWrapper<>();
+        if (questionQueryRequest == null) {
+            return queryWrapper;
+        }
+
+        Long id = questionQueryRequest.getId();
+        String title = questionQueryRequest.getTitle();
+        String content = questionQueryRequest.getContent();
+        String answer = questionQueryRequest.getAnswer();
+        List<String> tags = questionQueryRequest.getTags();
+        Integer submitNum = questionQueryRequest.getSubmitNum();
+        Integer acceptedNum = questionQueryRequest.getAcceptedNum();
+        Long userId = questionQueryRequest.getUserId();
+        String sortField = questionQueryRequest.getSortField();
+        String sortOrder = questionQueryRequest.getSortOrder();
+
+        // 拼接查询条件
+        queryWrapper.like(StringUtils.isNotBlank(title), "title", title);
+        queryWrapper.like(StringUtils.isNotBlank(content), "content", content);
+        queryWrapper.like(StringUtils.isNotBlank(answer), "answer", answer);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(id), "id", id);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+
+        if (CollUtil.isNotEmpty(tags)) {
+            for (String tag : tags) {
+                queryWrapper.like("tags", "\"" + tag + "\"");
+            }
+        }
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+//    @Override
+//    public Page<Question> searchFromEs(QuestionQueryRequest questionQueryRequest) {
+//        Long id = questionQueryRequest.getId();
+//        Long notId = questionQueryRequest.getNotId();
+//        String searchText = questionQueryRequest.getSearchText();
+//        String title = questionQueryRequest.getTitle();
+//        String content = questionQueryRequest.getContent();
+//        List<String> tagList = questionQueryRequest.getTags();
+//        List<String> orTagList = questionQueryRequest.getOrTags();
+//        Long userId = questionQueryRequest.getUserId();
+//        // es 起始页为 0
+//        long current = questionQueryRequest.getCurrent() - 1;
+//        long pageSize = questionQueryRequest.getPageSize();
+//        String sortField = questionQueryRequest.getSortField();
+//        String sortOrder = questionQueryRequest.getSortOrder();
+//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+//        // 过滤
+//        boolQueryBuilder.filter(QueryBuilders.termQuery("isDelete", 0));
+//        if (id != null) {
+//            boolQueryBuilder.filter(QueryBuilders.termQuery("id", id));
+//        }
+//        if (notId != null) {
+//            boolQueryBuilder.mustNot(QueryBuilders.termQuery("id", notId));
+//        }
+//        if (userId != null) {
+//            boolQueryBuilder.filter(QueryBuilders.termQuery("userId", userId));
+//        }
+//        // 必须包含所有标签
+//        if (CollUtil.isNotEmpty(tagList)) {
+//            for (String tag : tagList) {
+//                boolQueryBuilder.filter(QueryBuilders.termQuery("tags", tag));
+//            }
+//        }
+//        // 包含任何一个标签即可
+//        if (CollUtil.isNotEmpty(orTagList)) {
+//            BoolQueryBuilder orTagBoolQueryBuilder = QueryBuilders.boolQuery();
+//            for (String tag : orTagList) {
+//                orTagBoolQueryBuilder.should(QueryBuilders.termQuery("tags", tag));
+//            }
+//            orTagBoolQueryBuilder.minimumShouldMatch(1);
+//            boolQueryBuilder.filter(orTagBoolQueryBuilder);
+//        }
+//        // 按关键词检索
+//        if (StringUtils.isNotBlank(searchText)) {
+//            boolQueryBuilder.should(QueryBuilders.matchQuery("title", searchText));
+//            boolQueryBuilder.should(QueryBuilders.matchQuery("description", searchText));
+//            boolQueryBuilder.should(QueryBuilders.matchQuery("content", searchText));
+//            boolQueryBuilder.minimumShouldMatch(1);
+//        }
+//        // 按标题检索
+//        if (StringUtils.isNotBlank(title)) {
+//            boolQueryBuilder.should(QueryBuilders.matchQuery("title", title));
+//            boolQueryBuilder.minimumShouldMatch(1);
+//        }
+//        // 按内容检索
+//        if (StringUtils.isNotBlank(content)) {
+//            boolQueryBuilder.should(QueryBuilders.matchQuery("content", content));
+//            boolQueryBuilder.minimumShouldMatch(1);
+//        }
+//        // 排序
+//        SortBuilder<?> sortBuilder = SortBuilders.scoreSort();
+//        if (StringUtils.isNotBlank(sortField)) {
+//            sortBuilder = SortBuilders.fieldSort(sortField);
+//            sortBuilder.order(CommonConstant.SORT_ORDER_ASC.equals(sortOrder) ? SortOrder.ASC : SortOrder.DESC);
+//        }
+//        // 分页
+//        PageRequest pageRequest = PageRequest.of((int) current, (int) pageSize);
+//        // 构造查询
+//        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(boolQueryBuilder)
+//                .withPageable(pageRequest).withSorts(sortBuilder).build();
+//        SearchHits<QuestionEsDTO> searchHits = elasticsearchRestTemplate.search(searchQuery, QuestionEsDTO.class);
+//        Page<Question> page = new Page<>();
+//        page.setTotal(searchHits.getTotalHits());
+//        List<Question> resourceList = new ArrayList<>();
+//        // 查出结果后，从 db 获取最新动态数据（比如点赞数）
+//        if (searchHits.hasSearchHits()) {
+//            List<SearchHit<QuestionEsDTO>> searchHitList = searchHits.getSearchHits();
+//            List<Long> questionIdList = searchHitList.stream().map(searchHit -> searchHit.getContent().getId())
+//                    .collect(Collectors.toList());
+//            List<Question> questionList = baseMapper.selectBatchIds(questionIdList);
+//            if (questionList != null) {
+//                Map<Long, List<Question>> idQuestionMap = questionList.stream().collect(Collectors.groupingBy(Question::getId));
+//                questionIdList.forEach(questionId -> {
+//                    if (idQuestionMap.containsKey(questionId)) {
+//                        resourceList.add(idQuestionMap.get(questionId).get(0));
+//                    } else {
+//                        // 从 es 清空 db 已物理删除的数据
+//                        String delete = elasticsearchRestTemplate.delete(String.valueOf(questionId), QuestionEsDTO.class);
+//                        log.info("delete question {}", delete);
+//                    }
+//                });
+//            }
+//        }
+//        page.setRecords(resourceList);
+//        return page;
+//    }
+
+    @Override
+    public QuestionVO getQuestionVO(Question question, HttpServletRequest request) {
+        QuestionVO questionVO = QuestionVO.objToVo(question);
+        // 1. 关联查询用户信息
+        Long userId = question.getUserId();
+        User user = null;
+        if (userId != null && userId > 0) {
+            user = userService.getById(userId);
+        }
+        UserVO userVO = userService.getUserVO(user);
+        questionVO.setUserVO(userVO);
+        // 2. 已登录，获取用户点赞、收藏状态
+//        User loginUser = userService.getLoginUserPermitNull(request);
+//        if (loginUser != null) {
+//            // 获取点赞
+//            QueryWrapper<QuestionThumb> questionThumbQueryWrapper = new QueryWrapper<>();
+//            questionThumbQueryWrapper.in("questionId", questionId);
+//            questionThumbQueryWrapper.eq("userId", loginUser.getId());
+//            QuestionThumb questionThumb = questionThumbMapper.selectOne(questionThumbQueryWrapper);
+//            questionVO.setHasThumb(questionThumb != null);
+//            // 获取收藏
+//            QueryWrapper<QuestionFavour> questionFavourQueryWrapper = new QueryWrapper<>();
+//            questionFavourQueryWrapper.in("questionId", questionId);
+//            questionFavourQueryWrapper.eq("userId", loginUser.getId());
+//            QuestionFavour questionFavour = questionFavourMapper.selectOne(questionFavourQueryWrapper);
+//            questionVO.setHasFavour(questionFavour != null);
+//        }
+        return questionVO;
+    }
+
+    @Override
+    public Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request) {
+        List<Question> questionList = questionPage.getRecords();
+        Page<QuestionVO> questionVOPage = new Page<>(questionPage.getCurrent(), questionPage.getSize(), questionPage.getTotal());
+        if (CollUtil.isEmpty(questionList)) {
+            return questionVOPage;
+        }
+        // 1. 关联查询用户信息
+        Set<Long> userIdSet = questionList.stream().map(Question::getUserId).collect(Collectors.toSet());
+        Map<Long, List<User>> userIdUserListMap = userService.listByIds(userIdSet).stream()
+                .collect(Collectors.groupingBy(User::getId));
+        // 2. 已登录，获取用户点赞、收藏状态
+//        Map<Long, Boolean> questionIdHasThumbMap = new HashMap<>();
+//        Map<Long, Boolean> questionIdHasFavourMap = new HashMap<>();
+//        User loginUser = userService.getLoginUserPermitNull(request);
+//        if (loginUser != null) {
+//            Set<Long> questionIdSet = questionList.stream().map(Question::getId).collect(Collectors.toSet());
+//            loginUser = userService.getLoginUser(request);
+//            // 获取点赞
+//            QueryWrapper<QuestionThumb> questionThumbQueryWrapper = new QueryWrapper<>();
+//            questionThumbQueryWrapper.in("questionId", questionIdSet);
+//            questionThumbQueryWrapper.eq("userId", loginUser.getId());
+//            List<QuestionThumb> questionQuestionThumbList = questionThumbMapper.selectList(questionThumbQueryWrapper);
+//            questionQuestionThumbList.forEach(questionQuestionThumb -> questionIdHasThumbMap.put(questionQuestionThumb.getQuestionId(), true));
+//            // 获取收藏
+//            QueryWrapper<QuestionFavour> questionFavourQueryWrapper = new QueryWrapper<>();
+//            questionFavourQueryWrapper.in("questionId", questionIdSet);
+//            questionFavourQueryWrapper.eq("userId", loginUser.getId());
+//            List<QuestionFavour> questionFavourList = questionFavourMapper.selectList(questionFavourQueryWrapper);
+//            questionFavourList.forEach(questionFavour -> questionIdHasFavourMap.put(questionFavour.getQuestionId(), true));
+//        }
+        // 填充信息
+        List<QuestionVO> questionVOList = questionList.stream()
+                .map(question -> {
+                    QuestionVO questionVO = QuestionVO.objToVo(question);
+                    Long userId = question.getUserId();
+                    User user = null;
+                    if (userIdUserListMap.containsKey(userId)) {
+                        user = userIdUserListMap.get(userId).get(0);
+                    }
+                    questionVO.setUserVO(userService.getUserVO(user));
+        //            questionVO.setHasThumb(questionIdHasThumbMap.getOrDefault(question.getId(), false));
+        //            questionVO.setHasFavour(questionIdHasFavourMap.getOrDefault(question.getId(), false));
+                    return questionVO;
+                })
+                .collect(Collectors.toList());
+        questionVOPage.setRecords(questionVOList);
+        return questionVOPage;
+    }
 }
 
 
--- src/main/java/com/yupi/oj/service/impl/QuestionSubmitServiceImpl.java
+++ src/main/java/com/yupi/oj/service/impl/QuestionSubmitServiceImpl.java
@@ -1,10 +1,44 @@
 package com.yupi.oj.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yupi.oj.common.ErrorCode;
+import com.yupi.oj.constant.CommonConstant;
+import com.yupi.oj.exception.BusinessException;
+import com.yupi.oj.model.dto.question.QuestionQueryRequest;
+import com.yupi.oj.model.dto.questionsubmit.JudgeInfo;
+import com.yupi.oj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.yupi.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
+import com.yupi.oj.model.entity.Question;
 import com.yupi.oj.model.entity.QuestionSubmit;
+import com.yupi.oj.model.entity.QuestionSubmit;
+import com.yupi.oj.model.entity.User;
+import com.yupi.oj.model.enums.QuestionSubmitLanguageEnum;
+import com.yupi.oj.model.enums.QuestionSubmitStatusEnum;
+import com.yupi.oj.model.vo.QuestionSubmitVO;
+import com.yupi.oj.model.vo.QuestionVO;
+import com.yupi.oj.model.vo.UserVO;
+import com.yupi.oj.service.QuestionService;
+import com.yupi.oj.service.QuestionSubmitService;
 import com.yupi.oj.service.QuestionSubmitService;
 import com.yupi.oj.mapper.QuestionSubmitMapper;
+import com.yupi.oj.service.UserService;
+import com.yupi.oj.utils.SqlUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
 * @author guyingfan
@@ -14,6 +48,171 @@ import org.springframework.stereotype.Service;
 @Service
 public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper, QuestionSubmit>
     implements QuestionSubmitService{
+    @Resource
+    private QuestionService questionService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 题目提交
+     * @param questionSubmitAddRequest
+     * @param loginUser
+     * @return
+     */
+    @Override
+    public long doQuestionSubmit(QuestionSubmitAddRequest questionSubmitAddRequest, User loginUser) {
+        // 判断实体是否存在，根据类别获取实体
+        long questionId = questionSubmitAddRequest.getQuestionId();
+        Question question = questionService.getById(questionId);
+        if (question == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        // 提交题目
+        long userId = loginUser.getId();
+        String code = questionSubmitAddRequest.getCode();
+        // 检查编程语言是否合法
+        String language = questionSubmitAddRequest.getLanguage();
+        QuestionSubmitLanguageEnum languageEnum = QuestionSubmitLanguageEnum.getEnumByValue(language);
+        if(languageEnum == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "编程语言错误");
+        }
+
+        QuestionSubmit questionSubmit = new QuestionSubmit();
+        questionSubmit.setUserId(userId);
+        questionSubmit.setQuestionId(questionId);
+        questionSubmit.setCode(code);
+        questionSubmit.setLanguage(language);
+        // 用户提交题目时，初始判题状态为“判题中”
+        questionSubmit.setStatus(QuestionSubmitStatusEnum.WAITING.getValue());
+        // 用户提交题目时，初始判题信息默认为空
+        questionSubmit.setJudgeInfo(JSONUtil.toJsonStr(new JudgeInfo()));
+        boolean save = this.save(questionSubmit);
+
+        // 题目提交失败，抛出异常
+        if (!save) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "题目提交插入失败");
+        }
+        // 题目提交成功，返回题目提交的id
+        return questionSubmit.getId();
+
+//        // 每个用户串行提交
+//        // 锁必须要包裹住事务方法
+//        QuestionSubmitService questionSubmitService = (QuestionSubmitService) AopContext.currentProxy();
+//        synchronized (String.valueOf(userId).intern()) {
+//            return questionSubmitService.doQuestionSubmitInner(userId, questionId);
+//        }
+
+    }
+
+    /**
+     * 封装了事务的方法
+     *
+     * @param userId
+     * @param questionId
+     * @return
+     */
+    // @Override
+    @Transactional(rollbackFor = Exception.class)
+    public int doQuestionSubmitInner(long userId, long questionId) {
+        QuestionSubmit questionSubmit = new QuestionSubmit();
+        questionSubmit.setUserId(userId);
+        questionSubmit.setQuestionId(questionId);
+        QueryWrapper<QuestionSubmit> thumbQueryWrapper = new QueryWrapper<>(questionSubmit);
+        QuestionSubmit oldQuestionSubmit = this.getOne(thumbQueryWrapper);
+        boolean result;
+        // 已提交
+        if (oldQuestionSubmit != null) {
+            result = this.remove(thumbQueryWrapper);
+            if (result) {
+                // 提交数 - 1
+                result = questionService.update()
+                        .eq("id", questionId)
+                        .gt("thumbNum", 0)
+                        .setSql("thumbNum = thumbNum - 1")
+                        .update();
+                return result ? -1 : 0;
+            } else {
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+            }
+        } else {
+            // 未提交
+            result = this.save(questionSubmit);
+            if (result) {
+                // 提交数 + 1
+                result = questionService.update()
+                        .eq("id", questionId)
+                        .setSql("thumbNum = thumbNum + 1")
+                        .update();
+                return result ? 1 : 0;
+            } else {
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+            }
+        }
+    }
+
+    /**
+     * 获取查询包装类 (根据前端传来的请求对象，得到 mybatis 框架支持的查询包装类（queryWrapper类）)
+     *
+     * @param questionSubmitQueryRequest
+     * @return
+     */
+    @Override
+    public QueryWrapper<QuestionSubmit> getQueryWrapper(QuestionSubmitQueryRequest questionSubmitQueryRequest) {
+        QueryWrapper<QuestionSubmit> queryWrapper = new QueryWrapper<>();
+        if (questionSubmitQueryRequest == null) {
+            return queryWrapper;
+        }
+
+        Long questionId = questionSubmitQueryRequest.getQuestionId();
+        Integer status = questionSubmitQueryRequest.getStatus();
+        String language = questionSubmitQueryRequest.getLanguage();
+        Long userId = questionSubmitQueryRequest.getUserId();
+        String sortField = questionSubmitQueryRequest.getSortField();
+        String sortOrder = questionSubmitQueryRequest.getSortOrder();
+
+        // 拼接查询条件
+        queryWrapper.eq(ObjectUtils.isNotEmpty(questionId), "questionId", questionId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        queryWrapper.eq(QuestionSubmitStatusEnum.getEnumByValue(status) != null, "status", status);
+        queryWrapper.like(StringUtils.isNotBlank(language), "language", language);
+        queryWrapper.eq("isDelete", false);
+
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+    @Override
+    public QuestionSubmitVO getQuestionSubmitVO(QuestionSubmit questionSubmit, User loginUser) {
+        QuestionSubmitVO questionSubmitVO = QuestionSubmitVO.objToVo(questionSubmit);
+        // 仅本人和管理员能够看到本题题目提交代码
+        long loginId = loginUser.getId();
+
+        // 如果当前查询题目者既不是管理员，又不是登录用户，则需要脱敏题目代码
+        if (!userService.isAdmin(loginUser) && questionSubmit.getUserId() != loginId) {
+            questionSubmitVO.setCode(null);
+        }
+        return questionSubmitVO;
+    }
+
+    @Override
+    public Page<QuestionSubmitVO> getQuestionSubmitVOPage(Page<QuestionSubmit> questionSubmitPage,  User loginUser) {
+        List<QuestionSubmit> questionSubmitList = questionSubmitPage.getRecords();
+        Page<QuestionSubmitVO> questionSubmitVOPage = new Page<>(questionSubmitPage.getCurrent(), questionSubmitPage.getSize(), questionSubmitPage.getTotal());
+        if (CollUtil.isEmpty(questionSubmitList)) {
+            return questionSubmitVOPage;
+        }
+
+        // 填充信息
+        List<QuestionSubmitVO> questionSubmitVOList = questionSubmitList.stream()
+                .map(question -> {
+                    return getQuestionSubmitVO(question, loginUser);
+                })
+                .collect(Collectors.toList());
+        questionSubmitVOPage.setRecords(questionSubmitVOList);
+        return questionSubmitVOPage;
+    }
 
 }
 
