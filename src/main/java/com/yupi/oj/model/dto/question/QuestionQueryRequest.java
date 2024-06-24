d186fbda2b2699fcf2aa8428a7c1e93224c34e1d
Eileen Gu <81970016+ygu-hub@users.noreply.github.com>
2024-06-24 21:44:24 +0800

    添加问题增删改查请求封装类，方便后端从前端请求体中获取对应数据。

---
+++ src/main/java/com/yupi/oj/model/dto/question/QuestionQueryRequest.java
@@ -0,0 +1,63 @@
+package com.yupi.oj.model.dto.question;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.yupi.oj.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 查询题目
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class QuestionQueryRequest extends PageRequest implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 题目答案
+     */
+    private String answer;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tags;
+
+    /**
+     * 题目提交数
+     */
+    private Integer submitNum;
+
+    /**
+     * 题目通过数
+     */
+    private Integer acceptedNum;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
