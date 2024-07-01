f84c5e84afac0a092f15d12c34d9a30a87530e2f
Eileen Gu <81970016+ygu-hub@users.noreply.github.com>
2024-07-01 18:30:10 +0800

    使用openapi-typescript-codegen library（https://github.com/ferdikoomen/openapi-typescript-codegen）进行 Axios 前端请求代码快速生成（根据后端 OpenAPI specification 自动生成 typescript 对应的请求代码）

---
+++ generated/models/QuestionQueryRequest.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type QuestionQueryRequest = {
+    acceptedNum?: number;
+    answer?: string;
+    content?: string;
+    current?: number;
+    id?: number;
+    pageSize?: number;
+    sortField?: string;
+    sortOrder?: string;
+    submitNum?: number;
+    tags?: Array<string>;
+    title?: string;
+    userId?: number;
+};
+
