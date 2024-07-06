07631740406d2b1780569878ece687afb91be3b7
Eileen Gu <81970016+ygu-hub@users.noreply.github.com>
2024-07-06 11:47:02 +0800

    MarkDown 文本编辑组件添加 mode 属性，父组件可提供子组件输入框和预览框属性，默认为split

--- src/components/MdEditor.vue
+++ src/components/MdEditor.vue
@@ -1,9 +1,15 @@
 <template>
-  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+  <Editor
+    :value="value"
+    :mode="mode"
+    :plugins="plugins"
+    style="min-width: 800px"
+    @change="handleChange"
+  />
 </template>
 
 <script setup lang="ts">
-// 代码编辑器组件：https://github.com/bytedance/bytemd?tab=readme-ov-file#write-a-plugin
+// MarkDown 文本编辑器组件：https://github.com/bytedance/bytemd?tab=readme-ov-file#write-a-plugin
 import gfm from "@bytemd/plugin-gfm";
 import highlight from "@bytemd/plugin-highlight";
 import { Editor, Viewer } from "@bytemd/vue-next";
@@ -21,18 +27,22 @@ const plugins = [
  */
 interface Props {
   value: string;
+  // 输入框和预览框属性（默认‘split’）
+  mode: string;
   handleChange: (value: string) => void;
 }
+
 // 给属性设置默认值（如果父组件没有传对应的值/方法内容）
 withDefaults(defineProps<Props>(), {
   value: "",
+  mode: () => "split",
   handleChange: (v: string) => {
     console.log(v);
   },
 });
 </script>
 
-<style scoped>
+<style>
 .bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right:last-child svg {
   display: none;
 }
